30ee8836b6a9d2d4dbf690d04e637a648230f389
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-08 18:37:16 +0500

    snail

---
+++ 4-kyu/1Snail/script.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1
+
+// array = [
+//    [1, 2, 3],
+//    [4, 5, 6],
+//    [7, 8, 9],
+// ];
+// let res = ""
+// array.forEach((v, i) => { // [0, 0, 0] [] []
+//    v.forEach((x, y) => { // 0, 0, 0
+//       y !== v.length - 1 ?res+= x : v.forEach(inner => {
+
+//       })
+//    })
+// })
+
+// console.log(res);
